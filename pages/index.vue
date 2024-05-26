b7f74290743eedbf6fd93af0e990de1bd83e142e
woori <woori.kim.se@gmail.com>
2024-05-26 14:29:50 +0900

    refactoring & 업데이트 삭제 기능 추가

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,76 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { useRouter } from 'vue-router';
-import { useTodoStore } from '~/stores/todoStore';
-
-const router = useRouter();
-const todoStore = useTodoStore();
+import { ref } from 'vue'
+import { useTodoStore } from '~/stores/todoStore'
 
 // Reactive variables to control the visibility of the input field and store input values
-const showInput = ref(false);
-const todoItem = ref('');
-const priority = ref('medium');
+const showInput = ref(false)
+const todoItem = ref('')
+const priority = ref('medium')
+const editIndex = ref(-1)
 
-// Method to toggle the visibility of the input field
+// Method to toggle the visibility of the input field and initialize input values
 const toggleInput = () => {
-  showInput.value = !showInput.value;
+  showInput.value = !showInput.value
+  editIndex.value = -1
+  todoItem.value = ''
+  priority.value = 'medium'
 }
 
 // Method to handle the submission of the todo item
-const addTodo = () => {
-  if (todoItem.value) {
-    todoStore.addTodo({
+const addOrUpdateTodo = () => {
+  if (!todoItem.value) {
+    alert('Please enter a todo item.')
+    return
+  }
+
+  if (editIndex.value === -1) {
+    addTodo({
       item: todoItem.value,
       priority: priority.value
-    });
-
-    // Clear the input fields
-    todoItem.value = '';
-    priority.value = 'medium';
-    showInput.value = false;
+    })
   } else {
-    alert('Please enter a todo item.');
+    updateTodo(editIndex.value, {
+      item: todoItem.value,
+      priority: priority.value
+    })
   }
+
+  toggleInput()
+}
+
+// Pure function to add a todo
+const addTodo = (todo: { item: string, priority: string }) => {
+  const todoStore = useTodoStore()
+  todoStore.addTodo(todo)
+}
+
+// Pure function to update a todo
+const updateTodo = (index: number, updatedTodo: { item: string, priority: string }) => {
+  const todoStore = useTodoStore()
+  todoStore.updateTodo(index, updatedTodo)
+}
+
+// Method to delete a todo item
+const deleteTodo = (index: number) => {
+  const todoStore = useTodoStore()
+  todoStore.deleteTodo(index)
 }
 
 // Handle keypress for Enter key to submit the form
 const handleKeypress = (event: KeyboardEvent) => {
   if (event.key === 'Enter') {
-    addTodo();
+    addOrUpdateTodo()
   }
 }
 
-// Method to navigate to the ABV page
-const navigateToABV = () => {
-  router.push('/abv');
+// Method to prepare editing a todo item
+const prepareEditTodo = (index: number) => {
+  const todoStore = useTodoStore()
+  const todo = todoStore.todos[index]
+  todoItem.value = todo.item
+  priority.value = todo.priority
+  showInput.value = true
+  editIndex.value = index
 }
 
 // Priority options
@@ -50,7 +78,7 @@ const priorityOptions = [
   { text: '높음', value: 'high' },
   { text: '중간', value: 'medium' },
   { text: '낮음', value: 'low' }
-];
+]
 </script>
 
 <template>
@@ -59,28 +87,30 @@ const priorityOptions = [
     <button @click="toggleInput">ADD</button>
     <!-- Conditionally render the input field based on the reactive variable -->
     <div v-if="showInput">
-      <input 
-        type="text" 
-        v-model="todoItem" 
-        placeholder="Enter todo item" 
+      <input
+        type="text"
+        v-model="todoItem"
+        placeholder="Enter todo item"
         @keypress="handleKeypress"
       />
       <select v-model="priority" @keypress="handleKeypress">
-        <option 
-          v-for="option in priorityOptions" 
-          :key="option.value" 
+        <option
+          v-for="option in priorityOptions"
+          :key="option.value"
           :value="option.value"
         >
           {{ option.text }}
         </option>
       </select>
-      <button @click="addTodo">Enter</button>
+      <button @click="addOrUpdateTodo">{{ editIndex.value === -1 ? 'Enter' : 'Update' }}</button>
     </div>
     <!-- Display the list of todo items -->
-    <div v-if="todoStore.todos.length > 0">
+    <div v-if="useTodoStore().todos.length > 0">
       <ul>
-        <li v-for="(todo, index) in todoStore.todos" :key="index">
+        <li v-for="(todo, index) in useTodoStore().todos" :key="index">
           {{ todo.item }} - {{ todo.priority }}
+          <button @click="prepareEditTodo(index)">Edit</button>
+          <button @click="deleteTodo(index)">Delete</button>
         </li>
       </ul>
     </div>
--- stores/todoStore.ts
+++ stores/todoStore.ts
@@ -5,8 +5,15 @@ export const useTodoStore = defineStore('todoStore', {
     todos: [] as { item: string, priority: string }[]
   }),
   actions: {
+    // 저장 기능 추가 
     addTodo(todo: { item: string, priority: string }) {
       this.todos.push(todo);
+    },
+    deleteTodo(index: number) {
+      this.todos.splice(index, 1)
+    },
+    updateTodo(index: number, updatedTodo: { item: string, priority: string }) {
+      this.todos[index] = updatedTodo
     }
   }
 });
