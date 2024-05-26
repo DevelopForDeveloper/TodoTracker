34ceac5d54c5b912c87e9d92ad4e30302f073e0c
woori <woori.kim.se@gmail.com>
2024-05-26 13:12:07 +0900

    lint 설정

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import { ref } from "vue";
+import axios from "axios";
+const router = useRouter();
+
+const navigateToABV = () => {
+  router.push('/abv');
+}
+
+</script>
+
+<template>
+  <div id="app"/>
+</template>
+
+<style>
+
+.btn-container {
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 400px;
+}
+
+.btn-start {
+  border-radius: 5%;
+  width: 200px;
+  height: 60px;
+  font-size: 24px;
+  background-color: #a5c4cc;
+  color: #4d769e;
+  font-family: "UhBeeMiMi";
+}
+/*스타일링. */
+</style>
